839d97fba05e9bc62d26165f00730459ddc99afd
saifshaikh <sshaikh4@ggc.edu>
2025-05-07 15:10:14 -0400

    most of the html/css, still have to make tweaks

--- myLofiCalculator/src/CalculatorPage.jsx
+++ myLofiCalculator/src/CalculatorPage.jsx
@@ -2,19 +2,35 @@
 
 function CalculatorPage(){
 
+
+    const display = document
+
     return(
         <header>
-            <div className="calculatorContainer">
-                <h1>hello</h1>
+
+            <div id="calculator">
+                <input id="display" readOnly></input>
+                <div id="keys">
+                    <button onClick={"appendToDisplay('+')"} className="operator-btn">+</button>
+                    <button onClick={"appendToDisplay('7')"}>7</button>
+                    <button onClick={"appendToDisplay('8')"}>8</button>
+                    <button onClick={"appendToDisplay('9')"}>9</button>
+                    <button onClick={"appendToDisplay('-')"} className="operator-btn">-</button>
+                    <button onClick={"appendToDisplay('4')"}>4</button>
+                    <button onClick={"appendToDisplay('5')"}>5</button>
+                    <button onClick={"appendToDisplay('6')"}>6</button>
+                    <button onClick={"appendToDisplay('*')"} className="operator-btn">*</button>
+                    <button onClick={"appendToDisplay('1')"}>1</button>
+                    <button onClick={"appendToDisplay('2')"}>2</button>
+                    <button onClick={"appendToDisplay('3')"}>3</button>
+                    <button onClick={"appendToDisplay('/')"} className="operator-btn">/</button>
+                    <button onClick={"appendToDisplay('0')"}>0</button>
+                    <button onClick={"appendToDisplay('+')"}>.</button>
+                    <button onClick={"calculate()"}>=</button>
+                    <button onClick={"clearDisplay()"} className="operator-btn">C</button>
+                </div>
             </div>
 
-            <div className="musicContainer">
-
-            </div>
-
-            <div className="weatherContainer">
-
-            </div>
         </header>
     );
 }
